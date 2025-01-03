8e0f12308fa2a2793a17a5381ab14eecb42c9866
DevNahyanK <nahyangaming789@gmail.com>
2025-01-04 01:38:17 +0500

    update

--- next-sanity-blog/app/(blog)/layout.tsx
+++ next-sanity-blog/app/(blog)/layout.tsx
@@ -68,7 +68,7 @@ export default async function RootLayout({
     <html lang="en" className={`${inter.variable} bg-white text-black`}>
       <body>
         <section className="min-h-screen">
-          {isDraftMode && <AlertBanner />}
+          <AlertBanner />
           <main>{children}</main>
           <footer className="bg-accent-1 border-accent-2 border-t">
             <div className="container mx-auto px-5">
@@ -80,20 +80,20 @@ export default async function RootLayout({
               ) : (
                 <div className="flex flex-col items-center py-28 lg:flex-row">
                   <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-                    Built with Next.js.
+                    Blog website with sanity
                   </h3>
                   <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-                    <a
-                      href="https://nextjs.org/docs"
+                    <link
+                      href=""
                       className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
                     >
-                      Read Documentation
-                    </a>
+                      Thanks for visiting
+                    </link>
                     <a
-                      href="https://github.com/vercel/next.js/tree/canary/examples/cms-sanity"
+                      href="/"
                       className="mx-3 font-bold hover:underline"
                     >
-                      View on GitHub
+                      Back to Home
                     </a>
                   </div>
                 </div>
